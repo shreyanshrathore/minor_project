aaf8d38cc7dcb907829f8b97bdbafe4eeefa0309
shreyansh rathore <shreyanshrathore75649@gmail.com>
2022-10-31 11:09:19 +0530

    thjjsdh

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login';
+
+
+export default function App(){
+  return(
+      <div>
+      <Login />
+      
+      </div>
+  );
+}
+
+
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <div class="login-card">
+    <h2>Login</h2>
+    <h3>Enter your credentials</h3>
+    <form class="login-form">
+      <input type="text" id="clname" />
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <a href="https://website.com">Forgot your password?</a>
+      <button type="submit">LOGIN</button>
+    </form>
+  </div>
+  );
+}
+
+export default Login;
